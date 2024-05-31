ce6a76472d3d4dc3c46f040029f4490e44b128f1
naorb12 <naorb12@gmail.com>
2024-05-31 23:47:20 +0300

    Add project files.

---
+++ TestGit2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestGit2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
